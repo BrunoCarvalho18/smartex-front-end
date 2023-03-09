09ca61833ea8f606342f3a19b5938ae20e9c4944
Bruno Carvalho <32099971+BrunoCarvalho18@users.noreply.github.com>
2023-03-09 14:49:27 +0000

    first

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -11,16 +11,19 @@ describe('Login Test Suite', () => {
 
   it('login should be accessed sucessfully', () => {
        cy.login(mass_data.email,mass_data.password)
+       cy.wait(10000)
        cy.get("button[class='dropdown__toggle-button']").contains(mass_data.user)
   });
 
   it('login should  not be accessed sucessfully, because password or email is blank', () => {
     cy.login(mass_data.email," ")
+    cy.wait(10000)
     cy.get("li[class='form-error__list-item']").contains(mass_data.messageIncorrect)
   });
 
   it('login should  not be accessed sucessfully, because password or email is incorrect', () => {
     cy.login(mass_data.emailIncorrect,mass_data.password)
+    cy.wait(10000)
     cy.get("li[class='form-error__list-item']").contains(mass_data.messageIncorrect)
   });
 
